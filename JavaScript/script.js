91a075624d8d98fc12fd224c742b41f2c0062141
Techthoth <ayotomiwadavid@proton.me>
2024-07-17 08:47:56 +0100

    saving progress

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -7,9 +7,9 @@ menuToggler.addEventListener("click", () => {
   // Check if the sidebar has the 'active' class
   if (sideBar.classList.contains("active")) {
     // Change the image to 'close-icon.png' when the sidebar is active
-    menuToggler.src = "../images/close.png";
+    menuToggler.src = "../Images/close.png";
   } else {
     // Change the image back to 'menu-icon.png' when the sidebar is not active
-    menuToggler.src = "../images/menu.png";
+    menuToggler.src = "../Images/menu.png";
   }
 });
