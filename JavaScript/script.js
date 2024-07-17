5e4a9158a7225253b836abed4027fa5148d58d55
Techthoth <ayotomiwadavid@proton.me>
2024-07-17 08:43:05 +0100

    saving progress

---
+++ JavaScript/script.js
@@ -0,0 +1,15 @@
+const menuToggler = document.querySelector('#iconToggler');
+const sideBar = document.querySelector('.sideBar');
+
+menuToggler.addEventListener("click", () => {
+  sideBar.classList.toggle("active");
+
+  // Check if the sidebar has the 'active' class
+  if (sideBar.classList.contains("active")) {
+    // Change the image to 'close-icon.png' when the sidebar is active
+    menuToggler.src = "./images/close.png";
+  } else {
+    // Change the image back to 'menu-icon.png' when the sidebar is not active
+    menuToggler.src = "./images/menu.png";
+  }
+});
